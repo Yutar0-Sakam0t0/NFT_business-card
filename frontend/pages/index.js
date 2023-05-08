a8e0ebc91d26a68146fc545eaec0cf2267a1fbb6
unknown <sakamoto.yutaro@tis.co.jp>
2023-05-08 15:34:20 +0900

    commit0508

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
 
 //　サイトアクセス時のデータ取得
   useEffect(() => {
-    checkMetaMaskInstalled();
+    //checkMetaMaskInstalled();
 
     const account = JSON.parse(sessionStorage.getItem('account'));
     if(account){setAccount(account)};
@@ -382,7 +382,7 @@ export default function Home() {
           </div>
           
         ): isLoading ? (
-          <div className={"flex flex-col items-center bg-slate-100 text-blue-900 min-h-screen"}>
+          <div className={"flex flex-col items-center justify-center bg-slate-100 text-blue-900"}>
             <p className={"flex justify-center text-2xl font-bold my-8 mt-8"}>
               ブロックチェーンからデータ取得中...
             </p>
@@ -409,7 +409,7 @@ export default function Home() {
               ようこそ！
             </h2>
             <div className="px-2 py-2 mx-2 mb-10 bg-white border border-gray-400">
-              <span className="flex flex-col items-left font-semibold">
+              <span className="flex flex-col items-center justify-center font-semibold">
                 アドレス：{account}
               </span>
             </div>
@@ -419,11 +419,10 @@ export default function Home() {
                 ＜保有名刺一覧＞
               </span>
                 {cards.map((item, i) => (
-                  <div key={i} className="flex justify-left pl-1 py-2 mb-1">
+                  <div key={i} className="flex justify-center pl-1 py-2 mb-1">
                     <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-
                         <img
-                          className=" w-full h-48 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                          className=" w-full h-48 md:h-auto object-contain md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
                           src={item.imageURI}
                           alt=""
                         />
